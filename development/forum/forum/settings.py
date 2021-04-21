61e2f1a63920f1bbe7546748fa6ffbae85fad4c7
Michael Whitaker <miwhitaker@ymail.com>
2021-04-21 16:18:18 -0500

    added database requirements to django settings

--- development/forum/forum/settings.py
+++ development/forum/forum/settings.py
@@ -83,12 +83,12 @@ DATABASE_URL = os.environ['DATABASE_URL']
 
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
+#    }
+#}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
